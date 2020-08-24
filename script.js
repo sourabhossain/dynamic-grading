4f85a78927ab195bb174771cdea25ced0fbcc465
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-24 23:08:47 +0600

    add bangla button

---
+++ script.js
@@ -0,0 +1,9 @@
+// bangla result button
+document.querySelector("#banglaButton").addEventListener("click", () => {
+    const banglaMark = parseInt(document.querySelector("#banglaMark").value);
+    console.log(banglaMark);
+});
+
+// english result button
+
+// ict result button
