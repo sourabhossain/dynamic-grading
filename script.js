e1aa6439615a3cf10810eba7b622f442b0b02e4a
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-24 23:20:47 +0600

    add button one bug

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // bangla result button
 document.querySelector("#banglaButton").addEventListener("click", () => {
     const banglaMark = parseInt(document.querySelector("#banglaMark").value);
+    
+    if (!banglaMark) {
+        alert("empty!");
+    }
+
     console.log(banglaMark);
 });
 
 // english result button
 document.querySelector("#englishButton").addEventListener("click", () => {
     const englishMark = parseInt(document.querySelector("#englishMark").value);
+
+    if (!englishMark) {
+        alert("empty!");
+    }
+
     console.log(englishMark);
 });
 
 // ict result button
 document.querySelector("#ictButton").addEventListener("click", () => {
     const ictMark = parseInt(document.querySelector("#ictMark").value);
+
+    if (!ictMark) {
+        alert("empty!");
+    }
+
     console.log(ictMark);
 });
